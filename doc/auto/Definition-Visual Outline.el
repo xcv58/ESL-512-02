47f79ce7b3039af52165913ea5977180a61c2b9f
xcv58 <chenyihonglove@gmail.com>
2014-11-02 21:31:41 -0500

    init.

---
+++ doc/auto/Definition-Visual Outline.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook "Definition-Visual Outline"
+ (lambda ()
+    (TeX-run-style-hooks
+     "enumerate"
+     "fullpage"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
